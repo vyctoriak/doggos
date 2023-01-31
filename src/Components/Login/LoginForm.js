65e15c82dd0438274286191407aeecc6c9e02a8f
Vyctoria K <vyctoriak@gmail.com>
2023-01-31 22:24:55 +0100

    create useForm hook and using it on Input and LoginForm component

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import useForm from '../../hooks/useForm'
 import Button from '../Form/Button'
 import Input from '../Form/Input'
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const username = useForm()
+  const password = useForm()
 
   function handleSubmit(e) {
     e.preventDefault()
-    fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => {
-        console.log('RESPONSE =>', response)
-        return response.json()
+
+    if (username.validate() && password.validate()) {
+      fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
       })
-      .then((json) => {
-        console.log('JSON =>', json)
-      })
+        .then((response) => {
+          console.log('RESPONSE =>', response)
+          return response.json()
+        })
+        .then((json) => {
+          console.log('JSON =>', json)
+        })
+    }
   }
 
   return (
     <section>
       <h1>Sign in / Entrar</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="User" type="text" name="username" />
-        <Input label="Password" type="password" name="password" />
-        <Button type="submit">Login</Button>
+        <Input label="User" type="text" name="username" {...username} />
+        <Input label="Password" type="password" name="password" {...password} />
+        <Button>Login</Button>
       </form>
       <Link to="/login/create">Register</Link>
     </section>
