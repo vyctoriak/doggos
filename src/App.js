27efe569e2ff9e6e619b253f2cf6e0739778ad12
Vyctoria K <vyctoriak@gmail.com>
2023-01-31 20:26:50 +0100

    create header component and its style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './Global.css'
+import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
